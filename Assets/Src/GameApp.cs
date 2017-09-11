216aa81db056f89e6d05e4584c1b9a88dd000324
zd304 <568892758@qq.com>
2017-09-11 18:36:21 +0800

    demo

---
+++ Assets/Src/GameApp.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameApp : MonoBehaviour
+{
+	void Awake()
+	{
+		gameLoading = true;
+	}
+
+	void Start()
+	{
+		GameObject camera = Resources.Load<GameObject>("Prefab/MainCamera");
+		camera = GameObject.Instantiate(camera);
+		mainCamera = camera.GetComponent<Camera>();
+
+		screenHeight = mainCamera.orthographicSize;
+		screenWidth = mainCamera.aspect * screenHeight;
+
+		GameObject uiMain = Resources.Load<GameObject>("GUI/ui_main");
+		uiMain = GameObject.Instantiate(uiMain);
+		Canvas canvas = uiMain.GetComponent<Canvas>();
+		canvas.worldCamera = mainCamera;
+
+		GameObject spaceShip = Resources.Load<GameObject>("Prefab/spaceship");
+		player = GameObject.Instantiate(spaceShip);
+		player.transform.position = new Vector3(-(screenWidth - 2.0f), 0.0f, 1.0f);
+
+		gameLoading = false;
+	}
+
+	void Update()
+	{
+		
+	}
+
+	public static float screenWidth = 0.0f;
+	public static float screenHeight = 0.0f;
+
+	public static Camera mainCamera = null;
+	public static GameObject player = null;
+
+	public static bool gameLoading = true;
+}
