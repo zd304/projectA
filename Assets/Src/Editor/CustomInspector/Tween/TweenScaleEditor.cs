1d3f305d149e01e77ef85dc37d97e93f62ab291f
zd304 <568892758@qq.com>
2019-07-21 23:45:55 +0800

    发射鱼

---
+++ Assets/Src/Editor/CustomInspector/Tween/TweenScaleEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(TweenScale))]
+public class TweenScaleEditor : TweenBaseEditor
+{
+    public override void OnInspectorGUI()
+    {
+        TweenScale tween = target as TweenScale;
+        tween.from = EditorGUILayout.Vector3Field("From", tween.from);
+        tween.to = EditorGUILayout.Vector3Field("To", tween.to);
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Src/Tween/TweenScale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class TweenScale : TweenBase
+{
+    protected override void UpdateTween(float t)
+    {
+        transform.localScale = Lerp(from, to, t);
+    }
+
+    public Vector3 from = Vector3.zero;
+    public Vector3 to = Vector3.zero;    
+}
